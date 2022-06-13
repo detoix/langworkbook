d4db4b904b928847deff101f9759bcc5037edc2e
detoix <dk14yo@gmail.com>
2022-06-13 21:26:15 +0200

    pagination on front

--- client/src/components/Workbook.tsx
+++ client/src/components/Workbook.tsx
@@ -4,6 +4,8 @@ import { getExercises } from "../services/client"
 import { Excercise } from "../models/excercise"
 
 const Exercises = () => {
+
+  const navigate = useNavigate()
   const [searchParams, setSearchParams] = useSearchParams()
   const query = {
     offset: Number(searchParams.get("offset")),
@@ -17,10 +19,40 @@ const Exercises = () => {
         {(exercises: Excercise[]) => (
           <div>
             {exercises.map(exercise => 
-              <p>
-                {exercise.id}. {exercise.tags.map(tag => <a href="">#{tag}</a>)} {exercise.data.content.map(phrase => phrase.text).join("_")} <Link to={"/exercises/" + exercise.id}>[take]</Link>
+              <p key={exercise.id}>
+                {exercise.id}. {exercise.tags.map(tag => <a href="/workbook">#{tag}</a>)} {exercise.data.content.map(phrase => phrase.text).join("_")} <Link to={"/exercises/" + exercise.id}>[take]</Link>
               </p>
             )}
+
+            <button onClick={event => { 
+              event.preventDefault()
+              let params = new URLSearchParams(searchParams.toString());
+              params.set('offset', Math.max(query.offset - query.limit, 0).toString());
+              setSearchParams(params.toString());
+              navigate('/workbook?'+params.toString())
+              }}>prev</button>
+
+            <select defaultValue={query.limit} onChange={event => { 
+              let params = new URLSearchParams(searchParams.toString());
+              params.set('limit', event.target.value);
+              setSearchParams(params.toString());
+              navigate('/workbook?'+params.toString())
+              }}
+            >
+              <option>1</option>
+              <option>10</option>
+              <option>25</option>
+              <option>50</option>
+            </select>
+
+            <button onClick={event => { 
+              event.preventDefault()
+              let params = new URLSearchParams(searchParams.toString());
+              params.set('offset', (query.offset + query.limit).toString());
+              setSearchParams(params.toString());
+              navigate('/workbook?'+params.toString())
+              }}>next</button>
+
           </div>
         )}
       </Async.Fulfilled>
