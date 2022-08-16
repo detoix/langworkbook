54133a19ffc5cdb9ad5815ef407ccc49387b7b43
detoix <dk14yo@gmail.com>
2022-08-16 19:28:48 +0200

    cleanup

--- client/src/services/client.tsx
+++ client/src/services/client.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { IdCarrier, Excercise, NewExercise, ExerciseSolution } from "../models/excercise"
 import { Query } from "../models/query"
 
-const url = "https://green-sunset-2339.fly.dev"
+const url = "http://localhost:8080"
 
 const getExercises = async (query: Query) => {
   const searchParams = new URLSearchParams()
