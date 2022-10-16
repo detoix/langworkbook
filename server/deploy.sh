7a9b1de4f66f32a7f80b2f182165d5f3c6c66529
detoix <dk14yo@gmail.com>
2022-10-16 19:09:10 +0200

    cleanup

--- server/deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build -t registry.heroku.com/detoix-web-server/web . && heroku container:push web -a detoix-web-server && heroku container:release web -a detoix-web-server && docker image prune -f
